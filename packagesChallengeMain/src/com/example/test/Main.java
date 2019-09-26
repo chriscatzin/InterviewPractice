9e6d3d1955daf48a00fb89b4b0e5c9229e3f2c62
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-09-25 22:56:28 -0700

    Daily Practice

--- packagesChallengeMain/src/com/example/test/Main.java
+++ packagesChallengeMain/src/com/example/test/Main.java
@@ -1,8 +1,19 @@
 package com.example.test;
 
+import com.collabera.myLibrary.Series;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println(Series.nSum(i));
+        }
+
+        System.out.println("**************************");
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println(Series.factorial(i));
+        }
     }
 }
