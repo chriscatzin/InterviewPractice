e2e246e9764a6c238226cd92c65ed97674d44422
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-09-25 22:38:05 -0700

    Daily Practice

---
+++ packagesChallenge/src/com/collabera/myLibrary/Series.java
@@ -0,0 +1,14 @@
+package com.collabera.myLibrary;
+
+public class Series {
+
+    public static long nSum(int n){
+        return (n * (n + 1)) / 2;
+    }
+
+    public static long factorial(int n){
+        if(n == 0){
+            return 0;
+        }
+    }
+}
