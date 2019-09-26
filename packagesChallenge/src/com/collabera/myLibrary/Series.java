4b473d837b1e833b6612a21503710c431512a458
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-09-25 22:41:43 -0700

    Daily Practice

--- packagesChallenge/src/com/collabera/myLibrary/Series.java
+++ packagesChallenge/src/com/collabera/myLibrary/Series.java
@@ -10,5 +10,24 @@ public class Series {
         if(n == 0){
             return 0;
         }
+        long fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact *= i;
+        }
+        return fact;
     }
+
+    public static long fibonacci(int n){
+        if(n == 0){
+            return 0;
+        } else if(n == 1){
+            return 1;
+        }
+        long nMinus1 = 1;
+        long nMinus2 = 0;
+        for(int i = 1; i < n; i++){
+
+        }
+    }
+
 }
