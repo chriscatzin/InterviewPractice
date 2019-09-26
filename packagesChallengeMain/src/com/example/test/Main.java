e7462b56e06beb402fe44137fadf08038532d499
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-09-25 22:58:05 -0700

    Daily Practice

--- packagesChallengeMain/src/com/example/test/Main.java
+++ packagesChallengeMain/src/com/example/test/Main.java
@@ -15,5 +15,14 @@ public class Main {
         for(int i = 0; i <= 10; i++){
             System.out.println(Series.factorial(i));
         }
+
+        System.out.println("**************************");
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println(Series.fibonacci(i));
+        }
+
+
+
     }
 }
