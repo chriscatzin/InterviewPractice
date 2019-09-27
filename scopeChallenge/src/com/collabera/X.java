04a1eb1131ea5dc83c06953edf113833e4a5ae11
ckc196@humboldt.edu <ckc196@humboldt.edu>
2019-09-26 22:35:48 -0700

    Daily Practice

--- scopeChallenge/src/com/collabera/X.java
+++ scopeChallenge/src/com/collabera/X.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class X {
     public class X {
-        
+        public class X {
+            private int x;
+
+            public X(Scanner x) {
+                System.out.println("Please enter a number: ");
+                this.x = x.nextInt();
+            }
+            
+        }
+
     }
 
 }
